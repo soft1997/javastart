64d69e4eaede21cc6b827b76016eb34dafa9e8d0
soft1997 <58154820+soft1997@users.noreply.github.com>
2022-03-21 00:18:57 +0800

    Update mytest.java

--- src/main/java/mytest.java
+++ src/main/java/mytest.java
@@ -2,6 +2,7 @@ public class mytest {
     public static void main(String[] args) {
         System.out.println("13");
         System.out.println("Sdad");
+            System.out.println("Sdad");
 
     }
 }
